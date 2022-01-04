f446d506637bade06776c72071cb5603b5f84f79
George Pastushok <gpastushok@yahoo.com>
2022-01-03 22:31:56 -0800

    changed boolean checks from '==' to '==='

--- Develop/script.js
+++ Develop/script.js
@@ -22,7 +22,7 @@ function writePassword() {
 
 function generatePassword() {
   //ask the user for the password's criteria
-  var length = prompt("what is the minimum length of password?","type a number between 8-128");
+  var length = prompt("what is the minimum length of password?", "type a number between 8-128");
   //parse the user's input to make sure a valid number was entered.
   // if (!(parseInt(length) > 8 && parseInt(length) < 128)) {
   //   length = prompt("Please enter a valid number","type a number between 8-128")
@@ -31,32 +31,33 @@ function generatePassword() {
   var capitals = confirm("does your password need to include capitals?");
   var numbers = confirm("does your password need to include numbers?");
   var special = confirm("does your password need to include special characters?");
-  
+
   //clear the arrayValid variable
   arrayValid = "";
 
   // fill the variable arrayValid with the user's criteria
-  if (lowercase == true) {
-  arrayValid += arrayLower;
+  if (lowercase === true) {
+    arrayValid += arrayLower;
   }
-  if (capitals == true) {
+  if (capitals === true) {
     arrayValid += arrayUpper;
   }
-  if (numbers == true) {
+  if (numbers === true) {
     arrayValid += arrayNum;
   }
-  if (special == true) {
+  if (special === true) {
     arrayValid += arraySpec;
   }
- 
-  //clear the password variable
- password = "";
 
- //create a routine that takes the pass length, approved string and creates a password
+  //clear the password variable
+  password = "";
+
+  //create a routine that takes the pass length, approved string and creates a password
   //using a for loop, for each position in password length create a string from characters in the arrayValid string 
   for (var i = 0; i < length; i++) {
     //create a random number based on the length of arrayValid, to select a random character in the string
     //var random is a random number between 0 and the length of arrayValid
+    // window.crypto should be used as it's more secure
     var random = Math.floor(Math.random() * arrayValid.length);
     // add selected characters to the password variable
     password += arrayValid.substring(random, random + 1);
