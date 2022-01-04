ba312f03d62cc0234783caa257490bf2b75a4d66
George Pastushok <gpastushok@yahoo.com>
2022-01-03 17:06:20 -0800

    created variables and framework for the rest of the app

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,18 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var length = prompt("what is the minimum length of password?","eg: 8 characters")
+    if ( parseInt(length) !>= 8 && parseInt(length) !<= 128) {
+      length
+    };
+    var lowercase = confirm("does your password need to include lowercase?");
+    var capitals = confirm("does your password need to include capitals?");
+    var numbers = confirm("does your password need to include numbers?");
+    var special = confirm("does your password need to include special characters?");
+  console.log(password, length, lowercase, capitals, numbers, special)
+  return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
